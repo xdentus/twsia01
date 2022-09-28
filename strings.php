ec42d3826176265e0dfc486163247a75692c59f5
xdentus <xdentus@outlook.com>
2022-09-28 14:24:20 +0200

    php done

--- strings.php
+++ strings.php
@@ -1,27 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Strings | PHP</title>
 </head>
+
 <body>
     <?php
-        define("dobra_rada", "bez chleba to sie nie najesz");
-        echo dobra_rada."<br>";
-        echo strlen(dobra_rada)."<br>";
-        echo str_word_count(dobra_rada)."<br>";
-        echo strrev(dobra_rada)."<br>";
-        echo strpos(dobra_rada, "to")."<br>";
-        echo str_replace("chleba", "mięsa", dobra_rada)."<br>";
-        echo strtolower(dobra_rada)."<br>";
-        echo strtoupper(dobra_rada)."<br>";
-        echo ucwords(dobra_rada)."<br>";
-        echo trim(dobra_rada)."<br>";
-        echo strstr(dobra_rada, "nie")."<br>";
-        echo substr(dobra_rada, 10, 5)."<br>";
-        echo str_pad(dobra_rada, 37, " -_-", STR_PAD_BOTH)."<br>";
+    define("DOBRA_RADA", "Bez chleba, to się nie najesz.");
+    echo DOBRA_RADA . "<br>";
+    echo strlen(DOBRA_RADA) . "<br>";
+    echo str_word_count(DOBRA_RADA) . "<br>";
+    echo strrev(DOBRA_RADA) . "<br>";
+    echo strpos(DOBRA_RADA, "to") . "<br>";
+    echo str_replace("chleba", "mięsa", DOBRA_RADA) . "<br>";
+    echo strtolower(DOBRA_RADA) . "<br>";
+    echo strtoupper(DOBRA_RADA) . "<br>";
+    echo ucwords(DOBRA_RADA) . "<br>";
+    echo trim(DOBRA_RADA) . "<br>";
+    echo strstr(DOBRA_RADA, "nie") . "<br>";
+    echo substr(DOBRA_RADA, 10, 5) . "<br>";
+    echo str_pad(DOBRA_RADA, 37, " -_-", STR_PAD_BOTH) . "<br>";
     ?>
 </body>
+
 </html>
